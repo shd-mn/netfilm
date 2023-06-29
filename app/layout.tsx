55a3730177ddb97df10a782d6ccba2db374b298e
Shadman <shadman.alizada@gmail.com>
2023-06-29 23:44:55 +0400

    style: made main page min-height 100%

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
