5bdf8525add662bb8d0cc6caac871a5c190fceed
Shadman <shadman.alizada@gmail.com>
2023-07-02 23:43:48 +0400

    feat: used Promise All for multiple promises

--- container/home/index.tsx
+++ container/home/index.tsx
@@ -1,12 +1,29 @@
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/services/fetchData";
 import Hero from "./Hero";
-import Movies from "@/mocks/movies.json";
-import Popular from "./Popular";
-const HomePage = () => {
+import MoviesSection from "./MoviesSection";
+
+async function HomePage() {
+  const popularPromise = getPopularMovies();
+  const topRatedPromise = getTopRatedMovies();
+  const upComingPromise = getUpcomingMovies();
+
+  const [
+    { results: popularMovies },
+    { results: upComingMovies },
+    { results: topRatedMovies },
+  ] = await Promise.all([popularPromise, upComingPromise, topRatedPromise]);
+
   return (
-    <div className="">
-      <Hero movie={Movies.results[2]} />
-      <Popular movies={Movies} />
+    <div>
+      <Hero movie={popularMovies[0]} />
+      <MoviesSection title="Popular Movies" movies={popularMovies} />
+      <MoviesSection title="Upcoming Movies" movies={upComingMovies} />
+      <MoviesSection title="Top Rated Movies" movies={topRatedMovies} />
     </div>
   );
-};
+}
 export default HomePage;
