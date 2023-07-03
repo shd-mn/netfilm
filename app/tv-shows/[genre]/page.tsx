17e9f615b05baf47c51c0e7284aa692a00dfd4c2
Shadman <shadman.alizada@gmail.com>
2023-07-03 21:34:38 +0400

    feat: added Tv Shows and not-found page for it

---
+++ app/tv-shows/[genre]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { discoverMovies } from "@/services/fetchData";
+import MovieCard from "@/components/common/MovieCard";
+import NotFound from "./not-found";
+
+type Props = {
+  params: {
+    genre: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+const TvShowsPage: React.FC<Props> = async ({ params, searchParams }) => {
+  const id = searchParams.with_genres;
+  const { results } = await discoverMovies(`&with_genres=${id}&page=1`);
+
+  if (results.length === 0) {
+    return <NotFound genre={params.genre} />;
+  }
+
+  return (
+    <div className="container pt-20">
+      <h4 className="mb-12 mt-12 text-2xl">
+        {decodeURIComponent(params.genre)}
+      </h4>
+      <div className="grid grid-cols-6 gap-8">
+        {results.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TvShowsPage;
--- components/header/GenreList.tsx
+++ components/header/GenreList.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import { Genres } from "@/utils/genreProps";
 import Link from "next/link";
 type Props = {
+  pathname: string;
   genres: Genres[];
 };
-const GenreList: React.FC<Props> = ({ genres }) => {
+const GenreList: React.FC<Props> = ({ pathname, genres }) => {
   return (
     <div className="invisible absolute top-full grid w-96 grid-cols-2 gap-2  rounded-md bg-slate-600 p-2  group-hover:visible">
       {genres.map((item) => (
         <Link
+          key={item.id}
           href={{
-            pathname: `/movies/${item.name}/`,
+            pathname: `${pathname}${item.name}`,
             query: { with_genres: `${item.id}` },
           }}
           className="rounded-md px-4 py-2 hover:bg-slate-900"
-          key={item.id}
         >
           {item.name}
         </Link>
