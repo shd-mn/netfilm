1e4d1813bbacba6342b832838f7388ff6d82499e
Shadman <shadman.alizada@gmail.com>
2024-02-08 15:08:48 +0400

    feat: created SectionHeader component for Home page sections

---
+++ container/Home/components/SectionHeader.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { headerDataType } from "../constants";
+
+type PropTypes = {
+  data: headerDataType;
+  element?: ReactNode;
+};
+
+function SectionHeader({ data, element }: PropTypes) {
+  return (
+    <header className="mb-20 flex items-end justify-between gap-24">
+      <div className="">
+        <h2 className="mb-4 text-4xl font-bold">{data.title}</h2>
+        <p className="text-lg text-grey-60 w-4/5">{data.desc}</p>
+      </div>
+      {element && element}
+    </header>
+  );
+}
+
+export default SectionHeader;
