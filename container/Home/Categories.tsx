0c838994e043b6484e2ffa6f921d8360d239378c
Shadman <shadman.alizada@gmail.com>
2024-02-08 15:08:00 +0400

    feat: created Home page Categories section

---
+++ container/Home/Categories.tsx
@@ -0,0 +1,16 @@
+import SectionHeader from "./components/SectionHeader";
+import GenreSlider from "@/components/GenreSlider";
+import { categoriesHeader } from "./constants";
+import type { Genres } from "@/utils/genreTypes";
+
+function Categories({ genres }: { genres: Genres[] }) {
+  return (
+    <section className="mb-36">
+      <div className="container">
+        <SectionHeader data={categoriesHeader} />
+      </div>
+    </section>
+  );
+}
+
+export default Categories;
