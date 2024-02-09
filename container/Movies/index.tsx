77d4d3ed265a16b9293974ba77804387e75bf6d9
Shadman <shadman.alizada@gmail.com>
2024-02-09 21:23:20 +0400

    refactor: fetch data in Movies comp

--- container/Movies/index.tsx
+++ container/Movies/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { getMovieGenres, getPopularMovies } from "@/services/fetchData";
+import Hero from "@/components/Hero";
+import PopularMovies from "./PopularMovies";
+import MoviesGenre from "./MoviesGenre";
+import type { MovieTypes } from "@/utils/movieTypes";
+import type { GenreTypes } from "@/utils/genreTypes";
 
-function Moves() {
-  return <div>Moves</div>;
+async function Movies() {
+  const movieData = getPopularMovies();
+  const genreData = getMovieGenres();
+
+  const [{ results: popularMovies }, { genres: movieGenre }]: [
+    MovieTypes,
+    GenreTypes
+  ] = await Promise.all([movieData, genreData]);
+
+  return (
+    <main>
+      <Hero movies={popularMovies} />
+      <section className="mb-44">
+        <div className="container rounded-xl border-2 border-black-15 p-12">
+          <MoviesGenre genres={movieGenre} />
+          <PopularMovies movies={popularMovies} />
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default Moves;
+export default Movies;
